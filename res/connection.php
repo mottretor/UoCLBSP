dfd2e0758399296f354a28384ca049da95b8033b
PasinduPriyashan <pasindu95.m@gmail.com>
2017-08-19 19:56:44 +0530

    Updated Server Values

--- res/connection.php
+++ res/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$con=mysqli_connect("uoclspinstance.cabhxxryzggf.us-east-1.rds.amazonaws.com","uocroot","uocrootpass","uoclbsp_db");
+$con=mysqli_connect("uoclbspdbinstance.c5cec24wzera.us-east-1.rds.amazonaws.com","uocroot","uocrootpass","uoclbsp_db");
 
 if (mysqli_connect_errno($con)) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
