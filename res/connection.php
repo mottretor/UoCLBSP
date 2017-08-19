775438c657356b20f62705c05aaf624a1c1a1e75
PasinduPriyashan <pasindu95.m@gmail.com>
2017-08-19 21:15:28 +0530

    Test 4

--- res/connection.php
+++ res/connection.php
@@ -5,5 +5,4 @@ $con=mysqli_connect("uoclbspdbinstance.c5cec24wzera.us-east-1.rds.amazonaws.com"
 if (mysqli_connect_errno($con)) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-echo 'hari';
 ?>
