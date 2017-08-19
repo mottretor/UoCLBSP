5ef3dc8b5a259c88c85abcd3153fc8fbe9337428
PasinduPriyashan <pasindu95.m@gmail.com>
2017-08-19 21:13:57 +0530

    Test 3

--- res/getpolygon.php
+++ res/getpolygon.php
@@ -2,7 +2,7 @@
 require 'connection.php';
 
 $result = mysqli_query($con,"SELECT * FROM polygon");
-$row = mysqli_fetch_array($result);
+$row = mysqli_fetch_row($result);
 $data = $row[0];
 
 echo $data;
