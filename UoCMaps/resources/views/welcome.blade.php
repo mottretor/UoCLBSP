bbc759dc64eeb9d2d5ed20236d5b735259d3a31b
PasinduPriyashan <pasindu95.m@gmail.com>
2017-12-04 10:18:58 +0530

    Added Vendor

--- UoCMaps/resources/views/welcome.blade.php
+++ UoCMaps/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    sbfjdhfksdhjfs
                 </div>
 
                 <div class="links">
