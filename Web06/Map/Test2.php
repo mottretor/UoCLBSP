40b26266d8c7f76775f60b5dd9a665b4682562c9
PasinduPriyashan <pasindu95.m@gmail.com>
2017-12-04 09:36:13 +0530

    Laravel Start

--- Web06/Map/Test2.php
+++ Web06/Map/Test2.php
@@ -2,7 +2,7 @@
 
 set_time_limit(0);
 
-$PORT = 2017; //the port on which we are connecting to the "remote" machine
+$PORT = 1978; //the port on which we are connecting to the "remote" machine
 $HOST = "localhost"; //the ip of the remote machine (in this case it's the same machine)
 $sock = socket_create(AF_INET, SOCK_STREAM, 0) //Creating a TCP socket
 or die("error: could not create socket\n");
@@ -12,7 +12,7 @@ or die("error: could not connect to host\n");
 
 $text = "Thadiyaaaaaa"+'\r'; //the text we want to send to the server
 
-socket_sendto($sock, $text, strlen($text), MSG_EOF, '127.0.0.1', '2017');
+socket_sendto($sock, $text, strlen($text),512, '127.0.0.1', '1978');
 //socket_write($sock, $text . "\n", strlen($text) + 1) //Writing the text to the socket
 //  or die("error: failed to write to socket\n");
 
