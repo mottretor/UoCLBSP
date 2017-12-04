185db5644745fe66ff875460b33dd18c4b5255b9
PasinduPriyashan <pasindu95.m@gmail.com>
2017-12-04 10:34:56 +0530

    Revert "Added Vendor"
    This reverts commit bbc759dc64eeb9d2d5ed20236d5b735259d3a31b.

--- UoCMaps/resources/views/welcome.blade.php
+++ UoCMaps/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    sbfjdhfksdhjfs
+                    Laravel
                 </div>
 
                 <div class="links">
